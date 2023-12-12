89fdfafe112fa1237489cf63d9a926e56bf7b78e
Jason Yang <jamongx@gmail.com>
2023-12-11 18:49:11 -0800

    Changed RESTful API about Follow service to Template literal format

--- twitter-clone-react/src/services/FollowService.js
+++ twitter-clone-react/src/services/FollowService.js
@@ -1,10 +1,10 @@
 import customAxios from "./axiosConfig";
 import { Config } from "../utils/Config";
 
-export const getFollowers = (id) => customAxios.get(Config.FOLLOWERS_REST_API_URL + '/' + id)
+export const getFollowers = (id) => customAxios.get(`${Config.FOLLOWS_REST_API_URL}/${id}/followers`);
 
-export const getFollowing = (id) => customAxios.get(Config.FOLLOWING_REST_API_URL + '/' + id)
+export const getFollowing = (id) => customAxios.get(`${Config.FOLLOWS_REST_API_URL}/${id}/following`);
 
-export const saveFollowers = (follow) => customAxios.post(Config.FOLLOWS_REST_API_URL, follow)
+export const saveFollowers = (follow) => customAxios.post(Config.FOLLOWS_REST_API_URL, follow);
 
-export const deleteFollowers = (id) => customAxios.delete(Config.FOLLOWS_REST_API_URL + '/' + id)
+export const deleteFollowers = (id) => customAxios.delete(`${Config.FOLLOWS_REST_API_URL}/${id}`);
