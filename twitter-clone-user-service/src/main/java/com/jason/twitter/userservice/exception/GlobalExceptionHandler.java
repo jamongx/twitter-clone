c1ba45830295e8de352e4cae88a48a8153cb1e77
Jason Yang <jamongx@gmail.com>
2023-12-11 17:51:57 -0800

    Added logger to evey methods Renamed handleTodoAPIException to handleUserAPIException Changed the return ErrorDetils to ErrorDetailsDto in handleUserAPIException Added handleResponseNotFoundException Added handleDuplicatedResponseException Added handleGlobalException

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/GlobalExceptionHandler.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.jason.twitter.userservice.exception;
 
+import com.jason.twitter.userservice.dto.ErrorDetailsDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,16 +14,33 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(UserAPIException.class)
-    public ResponseEntity<ErrorDetails> handleTodoAPIException(UserAPIException exception,
-                                                               WebRequest webRequest) {
-
-        ErrorDetails errorDetails = new ErrorDetails(
-                LocalDateTime.now(),
-                exception.getMessage(),
-                webRequest.getDescription(false)
-        );
-
+    public ResponseEntity<ErrorDetailsDto> handleUserAPIException(UserAPIException exception, WebRequest request) {
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(LocalDateTime.now(), exception.getMessage(), request.getDescription(false));
+        logger.error("UserAPIException: {}", exception.getMessage());
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetailsDto> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        logger.error("ResourceNotFoundException: {}", ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ErrorDetailsDto> handleDuplicateResourceException(DuplicateResourceException ex, WebRequest request) {
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+        logger.error("DuplicateResourceException: {}", ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetailsDto> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(LocalDateTime.now(), "An error occurred", request.getDescription(false));
+        logger.error("Exception: {}", ex.getMessage());
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
