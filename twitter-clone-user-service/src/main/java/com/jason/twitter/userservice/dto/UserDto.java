901bfbadd7270490d403c93089a41365929634f1
Jason Yang <jamongx@gmail.com>
2023-12-11 16:07:35 -0800

    Separated UserProfile from User and refactored UserDto.

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/dto/UserDto.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/dto/UserDto.java
@@ -1,11 +1,8 @@
 package com.jason.twitter.userservice.dto;
 
-import com.jason.twitter.userservice.entity.Role;
 import lombok.*;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Set;
+
 
 @Getter
 @Setter
@@ -14,15 +11,7 @@ import java.util.Set;
 @ToString
 public class UserDto {
     private Long id;
-    private String avatarUrl;
     private String username;
     private String email;
-    private String displayName;
-    private String bio;
-    private boolean active;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDate birthDate;
-    private Set<Role> roles;
 }
 
--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/entity/UserProfile.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/entity/UserProfile.java
@@ -6,7 +6,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @Setter
@@ -17,28 +19,38 @@ import java.time.LocalDateTime;
 @Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@Table(name ="users")
-public class User {
+@Table(name ="user_profiles")
+public class UserProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String username;
-    @Column(nullable = false, unique = true)
-    private String email;
-    @ToString.Exclude
+    private String avatarUrl;
+    @Column(name = "display_name", nullable = false)
+    private String displayName;
+    private String bio;
     @Column(nullable = false)
-    private String password;
+    private boolean active = true;
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
     @CreatedDate
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
 
     @ToString.Exclude
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
-    private UserProfile userProfile;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private User user;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
 }
