ae524e6ddb96bedcb851f008babe1c0148e3e162
Jason Yang <jamongx@gmail.com>
2023-12-12 00:31:22 -0800

    Added "/v1" to RESTful API address" for versioning Integrated active and deactive RESTful API to status

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/controller/UserProfileController.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/controller/UserProfileController.java
@@ -1,6 +1,7 @@
 package com.jason.twitter.userservice.controller;
 
 import com.jason.twitter.userservice.dto.AvatarDto;
+import com.jason.twitter.userservice.dto.StatusDto;
 import com.jason.twitter.userservice.dto.UserProfileDto;
 import com.jason.twitter.userservice.service.UserProfileService;
 import lombok.AllArgsConstructor;
@@ -10,29 +11,19 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Map;
+
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/api/user_profiles")
+@RequestMapping("/api/v1/user_profiles")
 @AllArgsConstructor
 public class UserProfileController {
     private final UserProfileService userProfileService;
 
-    @PatchMapping("/{id}/active")
-    public ResponseEntity<UserProfileDto> activeUser(@PathVariable Long id) {
-        UserProfileDto updatedProfileUser = userProfileService.activeUser(id);
-        return ResponseEntity.ok(updatedProfileUser);
-    }
-
-    @PatchMapping("/{id}/deactive")
-    public ResponseEntity<UserProfileDto> deactivateUser(@PathVariable Long id) {
-        UserProfileDto updatedProfileUser = userProfileService.deactiveUser(id);
-        return ResponseEntity.ok(updatedProfileUser);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<UserProfileDto> updateProfile(@PathVariable Long id, @RequestBody UserProfileDto userProfileDto) {
-        UserProfileDto updatedUserProfileDto = userProfileService.updateProfile(userProfileDto);
-        return ResponseEntity.ok(updatedUserProfileDto);
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<StatusDto> updateUserStatus(@PathVariable Long id, @RequestBody boolean isActive) {
+        StatusDto updatedUserProfileStatus = userProfileService.updateStatus(id, isActive);
+        return ResponseEntity.ok(updatedUserProfileStatus);
     }
 
     @PatchMapping("/{id}/avatar")
@@ -42,6 +33,12 @@ public class UserProfileController {
         }
         AvatarDto updatedAvatar = userProfileService.updateAvatar(id, file);
         return ResponseEntity.ok(updatedAvatar);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserProfileDto> updateProfile(@PathVariable Long id, @RequestBody UserProfileDto userProfileDto) {
+        UserProfileDto updatedUserProfileDto = userProfileService.updateProfile(userProfileDto);
+        return ResponseEntity.ok(updatedUserProfileDto);
     }
 
 }
