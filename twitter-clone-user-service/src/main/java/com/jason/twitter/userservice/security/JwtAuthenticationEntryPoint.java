c2c6d9914556ac304482e927b017842a09be55f7
Jason Yang <jamongx@gmail.com>
2023-12-11 17:02:56 -0800

    1. Added logger for AuthException to common method

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/security/JwtAuthenticationEntryPoint.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/security/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,9 @@
 package com.jason.twitter.userservice.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,12 +12,17 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) throws IOException {
 
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
+
 }
 
