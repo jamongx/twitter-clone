6098874dd1f7fba3c3782277751d68aa01038565
Jason Yang <jamongx@gmail.com>
2023-12-12 00:35:10 -0800

    Integrated activeUser and deactiveUser methods to updateStatus. Deleted setUserProfileActiveStatus method Created new updateStatus and It updates active column one query Improve updateAvatar and It updates active column one query Removed unnecessory comments

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/impl/UserProfileServiceImpl.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/impl/UserProfileServiceImpl.java
@@ -1,11 +1,11 @@
 package com.jason.twitter.userservice.service.impl;
 
 import com.jason.twitter.userservice.dto.AvatarDto;
+import com.jason.twitter.userservice.dto.StatusDto;
 import com.jason.twitter.userservice.dto.UserProfileDto;
 import com.jason.twitter.userservice.entity.UserProfile;
 import com.jason.twitter.userservice.exception.ResourceNotFoundException;
 import com.jason.twitter.userservice.repository.UserProfileRepository;
-import com.jason.twitter.userservice.repository.UserRepository;
 import com.jason.twitter.userservice.service.UserProfileService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -26,45 +26,35 @@ public class UserProfileServiceImpl implements UserProfileService {
 
     private final String usersAvatarDir;
 
-    private UserRepository userRepository;
     private UserProfileRepository userProfileRepository;
 
     private ModelMapper modelMapper;
 
-    private void setUserProfileActiveStatus(Long id, Boolean status) {
-        UserProfile userProfile = userProfileRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("UserProfile not found with id : " + id));
-        userProfile.setActive(status);
-        userProfileRepository.save(userProfile);
-    }
-
     @Override
-    public UserProfileDto activeUser(Long id) {
-        setUserProfileActiveStatus(id, Boolean.TRUE);
-        UserProfile updatedUserProfile = userProfileRepository.findById(id).get();
-        return modelMapper.map(updatedUserProfile, UserProfileDto.class);
+    public StatusDto updateStatus(Long id, boolean isActive) {
+        int updatedRows = userProfileRepository.updateUserProfileActiveStatus(id, isActive);
+        if (updatedRows == 0) {
+            throw new ResourceNotFoundException("UserProfile not found with id : " + id);
+        }
+        return new StatusDto(isActive);
     }
 
-    @Override
-    public UserProfileDto deactiveUser(Long id) {
-        setUserProfileActiveStatus(id, Boolean.FALSE);
-        UserProfile updatedUserProfile = userProfileRepository.findById(id).get();
-        return modelMapper.map(updatedUserProfile, UserProfileDto.class);
-    }
-
-    // just uplaod an avatar image file not update user information
     @Override
     public AvatarDto updateAvatar(Long id, MultipartFile file) {
-        UserProfile userProfile = userProfileRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("UserProfile not found with id : " + id));
-
         if (file.isEmpty()) {
             throw new IllegalArgumentException("Empty file cannot be uploaded");
         }
 
-        String fileName = storeFile(file, userProfile.getUser().getId());
-        return new AvatarDto("/avatar/" + fileName);
+        String fileName = storeFile(file, id);
+        String avatarUrl = "/avatar/" + fileName;
+
+        int updatedRows = userProfileRepository.updateUserProfileAvatarURL(id, avatarUrl);
+        if (updatedRows == 0) {
+            throw new ResourceNotFoundException("UserProfile not found with id : " + id);
+        }
+        return new AvatarDto(avatarUrl);
     }
+
 
     private String storeFile(MultipartFile file, Long userId) throws RuntimeException {
         try {
@@ -104,7 +94,6 @@ public class UserProfileServiceImpl implements UserProfileService {
         return userId + dateFormatted + fileExtension;
     }
 
-
     @Override
     @Transactional
     public UserProfileDto updateProfile(UserProfileDto userProfileDto) {
@@ -117,14 +106,13 @@ public class UserProfileServiceImpl implements UserProfileService {
         return modelMapper.map(updatedUserProfile, UserProfileDto.class);
     }
 
+
     private void updateEntityFromDto(UserProfile userProfile, UserProfileDto userProfileDto) {
-        // 여기에 필요한 필드 업데이트 로직 추가
         userProfile.setAvatarUrl(userProfileDto.getAvatarUrl());
         userProfile.setDisplayName(userProfileDto.getDisplayName());
         userProfile.setBio(userProfileDto.getBio());
         userProfile.setActive(userProfileDto.isActive());
         userProfile.setBirthDate(userProfileDto.getBirthDate());
-        // 필요한 유효성 검증 로직 추가
     }
 
 }
