652f3a3797eff32c6157ea670e099db1353fea06
Jason Yang <jamongx@gmail.com>
2023-12-12 00:35:50 -0800

    Renamed ActiveDto to StatusDto

---
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/dto/StatusDto.java
@@ -0,0 +1,12 @@
+package com.jason.twitter.userservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class StatusDto {
+    private boolean active;
+}
