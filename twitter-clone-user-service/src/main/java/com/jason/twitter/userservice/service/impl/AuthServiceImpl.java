a37c4d7ec23678e81d1aa08e2dfff92d7d548221
Jason Yang <jamongx@gmail.com>
2023-12-11 17:43:29 -0800

    Seperated the register and login methods to improve readibility. Replaced hard coding like "to SecurityContants class" SecurityConstants.BEARER_TOKEN_TYPE

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/impl/AuthServiceImpl.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/impl/AuthServiceImpl.java
@@ -5,26 +5,28 @@ import com.jason.twitter.userservice.dto.LoginDto;
 import com.jason.twitter.userservice.dto.RegisterDto;
 import com.jason.twitter.userservice.entity.Role;
 import com.jason.twitter.userservice.entity.User;
+import com.jason.twitter.userservice.entity.UserProfile;
 import com.jason.twitter.userservice.exception.UserAPIException;
 import com.jason.twitter.userservice.mapper.UserMapper;
 import com.jason.twitter.userservice.repository.RoleRepository;
+import com.jason.twitter.userservice.repository.UserProfileRepository;
 import com.jason.twitter.userservice.repository.UserRepository;
 import com.jason.twitter.userservice.security.JwtTokenProvider;
+import com.jason.twitter.userservice.constants.SecurityConstants;
 import com.jason.twitter.userservice.service.AuthService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -32,66 +34,81 @@ import java.util.Set;
 public class AuthServiceImpl implements AuthService {
 
     private final String defaultAvatarUrl;
-
-    private ModelMapper modelMapper;
-
     private UserRepository userRepository;
+    private UserProfileRepository userProfileRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authenticationManager;
 
-    private JwtTokenProvider jwtTokenProvider;
+    private JwtTokenProvider tokenProvider;
+
+    @Transactional
     @Override
     public String register(RegisterDto registerDto) {
+        validateUserExistence(registerDto.getUsername(), registerDto.getEmail());
 
-        // check username is already exists in database
-        if(userRepository.existsByUsername(registerDto.getUsername())) {
-            throw new UserAPIException(HttpStatus.BAD_REQUEST, "Username already exists!");
-        }
+        User user = createUser(registerDto);
+        UserProfile userProfile = createUserProfile(registerDto);
 
-        // check email is already exists in database
-        if(userRepository.existsByEmail(registerDto.getEmail())) {
-            throw new UserAPIException(HttpStatus.BAD_REQUEST, "Email already exists!");
-        }
+        user.setUserProfile(userProfile);
+        userProfile.setUser(user);
 
-        User user = modelMapper.map(registerDto, User.class);
-        //user.setAvatarUrl("/images/twitter-default-avatar.jpg");
-        user.setAvatarUrl(defaultAvatarUrl);
-        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
-        user.setBirthDate(LocalDate.parse(registerDto.getBirthDate()));
-
-        Set<Role> roles = new HashSet<>();
-        Role userRole = roleRepository.findByRole("ROLE_USER");
-        roles.add(userRole);
-
-        user.setRoles(roles);
-        userRepository.save(user);
+        User createdUser = userRepository.save(user);
+        System.out.println(createdUser);
 
         return "User Registered Successfully!.";
     }
 
+    @Transactional
     @Override
     public JwtAuthResponse login(LoginDto loginDto) {
-
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 loginDto.getUsernameOrEmail(),
                 loginDto.getPassword()
         ));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtTokenProvider.generateToken(authentication);
+        String token = tokenProvider.generateToken(authentication);
 
-        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
-        Optional<User> userOptional = userRepository.findByUsernameOrEmail(loginDto.getUsernameOrEmail(),
-                loginDto.getUsernameOrEmail());
-        if (userOptional.isPresent()) {
-            User loggedInUser = userOptional.get();
-            jwtAuthResponse = UserMapper.mapToJwtAuthResponse(loggedInUser, token, "Bearer");
-            jwtAuthResponse.setId(loggedInUser.getId());
-            jwtAuthResponse.setUsername(loggedInUser.getUsername());
-            jwtAuthResponse.setRoles(loggedInUser.getRoles());
+        User user = getUserByUsernameOrEmail(loginDto.getUsernameOrEmail());
+        return UserMapper.mapToJwtAuthResponse(user, token, SecurityConstants.BEARER_TOKEN_TYPE);
+    }
+
+    private void validateUserExistence(String username, String email) {
+        if(userRepository.existsByUsername(username)) {
+            throw new UserAPIException(HttpStatus.BAD_REQUEST, "Username already exists!");
         }
 
-        return jwtAuthResponse;
+        if(userRepository.existsByEmail(email)) {
+            throw new UserAPIException(HttpStatus.BAD_REQUEST, "Email already exists!");
+        }
     }
+
+    private User createUser(RegisterDto registerDto) {
+        return User.builder()
+                .username(registerDto.getUsername())
+                .email(registerDto.getEmail())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
+                .build();
+    }
+
+    private UserProfile createUserProfile(RegisterDto registerDto) {
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByRole(SecurityConstants.ROLE_USER);
+        roles.add(userRole);
+
+        return UserProfile.builder()
+                .avatarUrl(defaultAvatarUrl)
+                .displayName(registerDto.getDisplayName())
+                .bio(registerDto.getBio())
+                .active(Boolean.TRUE)
+                .birthDate(LocalDate.parse(registerDto.getBirthDate()))
+                .roles(roles).build();
+    }
+
+    private User getUserByUsernameOrEmail(String usernameOrEmail) {
+        return userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail));
+    }
+
 }
