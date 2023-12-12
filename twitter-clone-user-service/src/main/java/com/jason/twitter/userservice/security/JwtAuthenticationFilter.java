a4cababc5ec4f68607bfcbbaf7fd46dbec005cad
Jason Yang <jamongx@gmail.com>
2023-12-11 17:01:45 -0800

    1. Added logger to try catch block in doFilterInternal 2. Changed "Bearer " (hard coding  to constant BEARER_PREFIX

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/security/JwtAuthenticationFilter.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/security/JwtAuthenticationFilter.java
@@ -4,6 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,50 +22,44 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
     private CustomUserDetailService userDetailService;
 
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, CustomUserDetailService userDetailService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userDetailService = userDetailService;
-    }
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            // Get JWT token from HTTP request
+            String token = getTokenFromRequest(request);
+            // Validate Token
+            if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+                // get username from token
+                String username = jwtTokenProvider.getUsername(token);
+                UserDetails userDetails = userDetailService.loadUserByUsername(username);
 
-        // Get JWT token from HTTP request
-        String token = getTokenFromRequest(request);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
 
-        // Validate Token
-        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
-            // get username from token
-
-            String username = jwtTokenProvider.getUsername(token);
-            UserDetails userDetails = userDetailService.loadUserByUsername(username);
-
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.getAuthorities()
-            );
-
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred in JWT authentication process: {}", e.getMessage(), e);
         }
-
         filterChain.doFilter(request, response);
-
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
-
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
         }
         return null;
     }
