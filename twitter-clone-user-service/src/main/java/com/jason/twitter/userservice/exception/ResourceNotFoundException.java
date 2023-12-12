091aa858f790879f64e40b283d7b435a61cf25fe
Jason Yang <jamongx@gmail.com>
2023-12-11 17:46:53 -0800

    Added  serialVersionUID

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/ResourceNotFoundException.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/ResourceNotFoundException.java
@@ -3,10 +3,16 @@ package com.jason.twitter.userservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception class for resource not found scenarios.
+ * This exception is thrown when a requested resource is not found in the system.
+ */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
 }
-
