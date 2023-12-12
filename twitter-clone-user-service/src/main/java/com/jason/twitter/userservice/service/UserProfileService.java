24d57b37c21fbc9026664969f6fc40bc5a21500b
Jason Yang <jamongx@gmail.com>
2023-12-12 00:32:06 -0800

    Integrated activeUser and deactiveUser methods to updateStatus

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/UserProfileService.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/service/UserProfileService.java
@@ -2,14 +2,14 @@ package com.jason.twitter.userservice.service;
 
 
 import com.jason.twitter.userservice.dto.AvatarDto;
+import com.jason.twitter.userservice.dto.StatusDto;
 import com.jason.twitter.userservice.dto.UserProfileDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserProfileService {
 
-    UserProfileDto activeUser(Long id);
 
-    UserProfileDto deactiveUser(Long id);
+    StatusDto updateStatus(Long id, boolean isActive);
 
     AvatarDto updateAvatar(Long id, MultipartFile file);
 
