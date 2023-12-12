63ce56db7d3141f329d432adbe0f1dfbe43d331a
Jason Yang <jamongx@gmail.com>
2023-12-11 17:34:10 -0800

    Created SecurityConstants class to avoid hard coding

---
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/constants/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.jason.twitter.userservice.constants;
+
+public class SecurityConstants {
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static final String BEARER_TOKEN_TYPE = "Bearer";
+    public static final String BEARER_TOKEN_PREFIX = "Bearer ";
+    public static final String HTTP_AUTHORIZATION_HEADER = "Authorization";
+
+}
