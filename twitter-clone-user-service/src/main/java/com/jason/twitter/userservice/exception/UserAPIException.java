8ed7621801cf9816445fc2c0fd2174e75a62e4d4
Jason Yang <jamongx@gmail.com>
2023-12-11 17:46:13 -0800

    Added  serialVersionUID Removed "String message" member

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/UserAPIException.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/exception/UserAPIException.java
@@ -1,13 +1,18 @@
 package com.jason.twitter.userservice.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class UserAPIException extends RuntimeException {
 
-    private HttpStatus status;
-    private String message;
+    private static final long serialVersionUID = 1L;
+
+    private final HttpStatus status;
+
+    public UserAPIException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
 }
+
