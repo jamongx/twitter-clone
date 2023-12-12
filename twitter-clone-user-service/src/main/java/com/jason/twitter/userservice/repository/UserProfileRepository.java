d116b9137638853b070afe35c21d051f96b04a1d
Jason Yang <jamongx@gmail.com>
2023-12-12 00:38:44 -0800

    Created two methods using @modifying and @Query to update a column at one time.

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/repository/UserProfileRepository.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/repository/UserProfileRepository.java
@@ -4,9 +4,20 @@ package com.jason.twitter.userservice.repository;
 import com.jason.twitter.userservice.entity.User;
 import com.jason.twitter.userservice.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Modifying
+    @Query("UPDATE UserProfile u SET u.active = :status WHERE u.id = :id")
+    int updateUserProfileActiveStatus(@Param("id") Long id, @Param("status") Boolean status);
+
+
+    @Modifying
+    @Query("UPDATE UserProfile u SET u.avatarUrl = :avatarUrl WHERE u.id = :id")
+    int updateUserProfileAvatarURL(@Param("id") Long id, @Param("avatarUrl") String avatarUrl);
 }
