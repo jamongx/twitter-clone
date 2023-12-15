3bda306d5a239a27d470081a88a553c9bb92b6ae
Jason Yang <jamongx@gmail.com>
2023-12-14 16:57:16 -0800

    Added @EntityListeners(AuditingEntityListener.class) annotation to add createdAt automatically

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/entity/Role.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/entity/Role.java
@@ -3,6 +3,7 @@ package com.jason.twitter.userservice.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @ToString
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name ="roles")
 public class Role {
 
