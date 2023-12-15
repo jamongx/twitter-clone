5fa97e0dd2f0f867dc0cb654de64283321c842ae
Jason Yang <jamongx@gmail.com>
2023-12-14 16:59:12 -0800

    Added the methods for user service such as mapToUser and mapToUserDto

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/mapper/UserMapper.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/mapper/UserMapper.java
@@ -2,7 +2,9 @@ package com.jason.twitter.userservice.mapper;
 
 import com.jason.twitter.userservice.dto.FollowerDto;
 import com.jason.twitter.userservice.dto.JwtAuthResponse;
+import com.jason.twitter.userservice.dto.UserDto;
 import com.jason.twitter.userservice.entity.User;
+import com.jason.twitter.userservice.entity.UserProfile;
 
 public class UserMapper {
     public static FollowerDto mapToFollowerDto(Long followerId, User user) {
@@ -14,6 +16,39 @@ public class UserMapper {
                 user.getUserProfile().getDisplayName(),
                 user.getUserProfile().getBio()
         );
+    }
+
+    public static User mapToUser(UserDto userDto) {
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .email(userDto.getEmail())
+                .build();
+
+        UserProfile userProfile = UserProfile.builder()
+                .avatarUrl(userDto.getAvatarUrl())
+                .displayName(userDto.getDisplayName())
+                .bio(userDto.getBio())
+                .active(userDto.getActive())
+                .birthDate(userDto.getBirthDate())
+                .build();
+
+        userProfile.setUser(user);
+        user.setUserProfile(userProfile);
+        return user;
+    }
+
+
+    public static UserDto mapToUserDto(User user) {
+        return new UserDto(
+                user.getId(),
+                user.getUsername(),
+                user.getUserProfile().getDisplayName(),
+                user.getEmail(),
+                user.getUserProfile().getBio(),
+                user.getUserProfile().getAvatarUrl(),
+                user.getUserProfile().isActive(),
+                user.getUserProfile().getBirthDate());
     }
 
     public static JwtAuthResponse mapToJwtAuthResponse(User user, String token, String tokenType) {
