2ebacc594a8fa3f80ec548c27e3d3bafa8d1e53b
Jason Yang <jamongx@gmail.com>
2023-12-14 17:18:39 -0800

    Added clearAutomactically = true option to @Modifying because of clearing persistence context

--- twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/repository/UserProfileRepository.java
+++ twitter-clone-user-service/src/main/java/com/jason/twitter/userservice/repository/UserProfileRepository.java
@@ -1,6 +1,7 @@
 package com.jason.twitter.userservice.repository;
 
 import com.jason.twitter.userservice.entity.UserProfile;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,12 +9,14 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
-    @Modifying
+
+    @Modifying(clearAutomatically = true)
     @Query("UPDATE UserProfile u SET u.active = :status WHERE u.id = :id")
-    int updateUserProfileActiveStatus(@Param("id") Long id, @Param("status") Boolean status);
+    int updateUserProfileActiveStatus(@Param("id") Long id, @Param("status") Boolean active);
 
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query("UPDATE UserProfile u SET u.avatarUrl = :avatarUrl WHERE u.id = :id")
     int updateUserProfileAvatarURL(@Param("id") Long id, @Param("avatarUrl") String avatarUrl);
+
 }
